4c22aa8d9a5596a9dca41a6e932c23c73315005d
Eder <chlrobert93@gmail.com>
2022-05-01 23:35:36 -0500

    Add project files.

---
+++ MiClasePersona/Persona.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+namespace MiClasePersona
+{
+     class Persona
+    {
+
+        //Caracteristicas de la persona
+        string nombre;
+        int ojos, brazos, piernas;
+        string color_ojos;
+        string color_pelo;
+
+
+       //Comportamiento mediante métodos
+       public void andar (int velocidad)
+        {
+            Console.WriteLine("La persona  anda a: " + velocidad + " por hora");
+        }
+         
+        //Método hablar
+        public void hablar()
+        {
+            Console.WriteLine("La persona esta hablando jojojo");
+        }
+
+
+        //Nos va devolver el atributo nombre
+        public string getNombre()
+        {
+            return nombre;
+        }
+
+        //Modificar, establecer el nombre de la persona 
+        public void setNombre(string nombre)
+        {
+            this.nombre = nombre;
+
+        }
+
+
+    }
+}
---
+++ MiClasePersona/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace MiClasePersona
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear un objeto instanciado
+            Persona p = new Persona();
+            p.hablar();
+
+            p.setNombre("Robert");
+            Console.WriteLine("EL Nombre: " + p.getNombre() + " ha sido modificado");
+
+            Console.ReadKey();
+        }
+    }
+}
